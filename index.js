1f79897a894a877df09233d7b0c3d1e22b74c707
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-02 11:55:58 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/users', (req, res) => {
+  res.send([(id = Date.now()), (name = 'danny')])
+})
+
+server.post('/api/register', (req, res) => {
+  req.users = { user: Lambda, password: school }
+  res.json(req.users)
+})
+
+server.post('/api/login', (req, res) => {
+  res.status(200).json({ message: 'Welcome Danny' })
+})
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
